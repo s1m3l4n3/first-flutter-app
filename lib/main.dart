eccf5a93d6782ffd5ffba7308cc960bf9e85cb7d
Linda Simelane <linda.simelane@momentum.co.za>
2022-06-04 21:14:11 +0200

    Added custom font

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,43 @@ void main() {
       appBar: AppBar(
         title: const Text('My Second App'),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-      body: const Center(
-        child: Text('hello ninjas!')
+      body: Center(
+        child: Text(
+          'hello ninjas!',
+          style: TextStyle(
+            fontFamily: 'IndieFlower',
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            letterSpacing: 2.0,
+            color: Colors.red[200]
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        backgroundColor: Colors.red[600],
         child: const Text('click'),
       ),
     )
   ));
 }
 
-// main method
-// runApp is the parent Widget that will run the app
-// MaterialApp and Scaffold for the material look & feel
+//  1. main method.
+//  2. runApp is the parent Widget that will run the app.
+
+/*
+ *  3. MaterialApp and Scaffold for the material look & feel.
+ *  https://api.flutter.dev/flutter/material/Scaffold-class.html
+ */
+
+/*
+ *  4. have a look at material design colours.
+ *  https://material.io/resources/color/#!/?view.left=0&view.right=0
+ */
+
+// 5. pubspec.yaml is the main dependencies file
+
+
 
